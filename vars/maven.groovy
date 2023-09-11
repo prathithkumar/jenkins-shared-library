123e634d31d487877cb112ec8ff60b22e3fda886
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-11 19:24:08 +0200

    practice shell

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -16,10 +16,10 @@ def call() {
                 }
             }
             
-            stage('Generating Artifacts') {
+            stage('Code Compile') {
                 steps {
                     sh "echo Generating Artifacts for $COMPONENT"
-                    sh "mvn clean package"
+                    sh "mvn clean compile"
 
                     }
                 }
