26aa6c3f31ee419c67abdb937d72bbcc222abc5b
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-11 21:29:11 +0200

    practice shell

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -31,7 +31,8 @@ def call() {
 
             stage('Sonar Checks') {
                 steps{
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+                    sh "env"
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login={SONAR_CRED_USR} -Dsonar.password={SONAR_CRED_PSW}"
                 }
             }
             }
