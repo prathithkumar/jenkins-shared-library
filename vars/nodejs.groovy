68e4a212ce2e8eec46e662d78ae8e605bed8c74a
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-11 18:42:50 +0200

    practice shell

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,7 +12,7 @@ def call() {
             stage('Lint Checks') {
                 steps {
                     script {
-                        lintChecks{}
+                        lintChecks()
                     }
                 }
             }
