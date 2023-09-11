06a0ef43aa53e8b810c0b6cf4da01bca904970c2
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-11 18:40:24 +0200

    practice shell

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,15 +1,19 @@
-def call(){
-
+def lintChecks() {
+            sh "echo Installing JSlist"
+            sh "npm i jslint"
+            sh "echo starting lintChecks...."
+            sh "node_modules/jslint/bin/jslint.js server.js || true"
+            sh "echo lintChecks completed"
+}
+def call() {
     pipeline {
         agent any 
         stages {
             stage('Lint Checks') {
                 steps {
-                    sh "echo Installing JSlist"
-                    sh "npm i jslint"
-                    sh "echo starting lintChecks ....."
-                    sh "node_modules/jslint/bin/jslint.js server.js || true"
-                    sh "echo linkChecks completed"
+                    script {
+                        lintChecks{}
+                    }
                 }
             }
             
