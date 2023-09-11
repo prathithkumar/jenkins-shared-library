9b3b9e2dc1ecc36db249436eb5c501072f506ce0
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-11 21:22:31 +0200

    practice shell

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,6 +8,10 @@ def lintChecks() {
 def call() {
     pipeline {
         agent any 
+        environment {
+            SONAR_URL = "172.31.25.77"
+            SONAR_CRED  = credentials('SONAR_CRED')
+        }
         stages {
             stage('Lint Checks') {
                 steps {
@@ -24,6 +28,12 @@ def call() {
 
                     }
                 }
+
+            stage('Sonar Checks') {
+                steps{
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+                }
+            }
             }
         }
     }
