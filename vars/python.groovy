4b6894eddacebbb683889da34c47cec0d8c1e81e
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-12 16:42:36 +0200

    practice shell

--- vars/python.groovy
+++ vars/python.groovy
@@ -7,6 +7,10 @@ def lintChecks() {
 def call() {
     pipeline {
         agent any 
+        environment {
+            SONAR_URL   = "172.31.25.77"
+            SONAR_CRED  = credentials('SONAR_CRED')
+        }
         stages {
             stage('Lint Checks') {
                 steps {
