9d8bfa535b7b6c8f8bd364c13545b04671e3079c
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-12 16:27:51 +0200

    practice shell

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -22,8 +22,11 @@ def call() {
             }
             stage('Sonar Checks') {
                 steps {
-                    sh "env"
+                    sh "cho starting Sonar Checks for ${COMPONENT}"
                     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
+                    sh "echo Sonar Checks ${COMPONENT} are completed"
                     }
                 }
             stage('Generating Artifacts') {
