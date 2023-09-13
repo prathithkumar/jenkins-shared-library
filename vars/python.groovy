371e25c6a43edf7627125d35e185e71ea5fd1952
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-13 20:13:38 +0200

    practice shell

--- vars/python.groovy
+++ vars/python.groovy
@@ -49,7 +49,18 @@ def call() {
                         }    
                     }
                 }
-            }    
+            }
+            stage('Check the Release') {
+                when {
+                    expression { env.TAG_NAME != null }
+                }
+                steps {
+                    script {
+                       env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+                       print UPLOAD_STATUS
+                    }
+                }
+            }                 
             stage('Generating Artifacts') {
                 steps {
                     sh "echo artifacts Generation Completed"
