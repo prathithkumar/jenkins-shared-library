b5883b0f2a6cfae78d4cc7ef503e34eaeecfcb8c
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-14 16:47:35 +0200

    practice shell

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,7 +2,6 @@ def call() {
     node {
         common.lintChecks()
         env.ARGS="-Dsonar.java.binaries=target/"
-        env.NEXUS_URL   = "172.31.18.210 "
         common.sonarChecks()
         common.testCases()
     }
