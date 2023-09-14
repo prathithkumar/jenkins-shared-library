2215db8dd6cab40e0b0319f3de283dead81fbbe2
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-14 17:25:34 +0200

    practice shell

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,5 +1,6 @@
 def call() {
     node {
+        git branch: 'main', url: "https://github.com/prathithkumar/${COMPONENT}.git"
         common.lintChecks()
         env.ARGS="-Dsonar.java.binaries=target/"
         common.sonarChecks()
